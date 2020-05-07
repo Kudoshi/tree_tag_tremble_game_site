350a5e5c8dbf5f3b038b6b0769b6efb3992ee70e
Teddeh Miracle <49321866+TeddehMiracle@users.noreply.github.com>
2020-05-07 13:32:14 -0400

    Update Burnin.lua
    Fixed the ability, now it does everything needed to be done

--- scripts/vscripts/abilities/Burnin.lua
+++ scripts/vscripts/abilities/Burnin.lua
@@ -7,7 +7,7 @@ function Burnin:OnAbilityPhaseStart()
 	local sound_precast = "Hero_Warlock.RainOfChaos_buildup"
 
 	-- Play precast sound
-	EmitSoundOn(sound_precast, caster)
+	EmitGlobalSound(sound_precast)
 
 	return true
 end
@@ -19,37 +19,41 @@ function Burnin:OnAbilityPhaseInterrupted()
 	local sound_precast = "Hero_Warlock.RainOfChaos_buildup"
 
 	-- Stop precast sound
-	StopSoundOn(sound_precast, caster)
+	StopGlobalSound(sound_precast)
 end
 
 
 function Burnin:OnSpellStart()
 
 	local caster = self:GetCaster()
-	local point = self:GetAbsOrigin()
+	local point = caster:GetAbsOrigin()
 	local sound_cast = "Hero_Warlock.RainOfChaos"
 	local particle_start = "particles/units/heroes/hero_warlock/warlock_rain_of_chaos_start.vpcf"
 	local particle_main = "particles/units/heroes/hero_warlock/warlock_rain_of_chaos.vpcf"
 	
 	local radius = 500
-	local duration = self:GetSpecialValueFor("duration")
-	local effect_delay = self:GetSpecialValueFor("effect_delay")
+	local effect_delay = 1
 	
 	local particle_start_fx = ParticleManager:CreateParticle(particle_start, PATTACH_ABSORIGIN, caster)
 	ParticleManager:SetParticleControl(particle_start_fx, 0, point)
 	
+	EmitGlobalSound(sound_cast)	
 	--destroy trees around self
 	GridNav:DestroyTreesAroundPoint(point, radius, true)
 	
 	Timers:CreateTimer(effect_delay, function()
 		
 		local particle_main_fx = ParticleManager:CreateParticle(particle_main, PATTACH_ABSORIGIN, caster)
-		ParticleManager:SetParticleControl(particle_main_fx, 0, target_point)
+		ParticleManager:SetParticleControl(particle_main_fx, 0, point)
 		ParticleManager:SetParticleControl(particle_main_fx, 1, Vector(radius, 0, 0))
 		ParticleManager:ReleaseParticleIndex(particle_main_fx)
 		
-		local hero = pickBadHero(caster:GetPlayerOwnerID(), "npc_dota_hero_doom_bringer")
+		local hero = PlayerResource:ReplaceHeroWith(caster:GetPlayerOwnerID(), "npc_dota_hero_doom_bringer", 100, 0)
+		DebugPrint(tostring(caster:GetPlayerOwnerID()))
 		hero:SetRespawnsDisabled(false)
-		hero:RemoveAbility("pickinferno")
-		end)
+		
+		if hero:HasAbility("pickinferno") then
+			hero:RemoveAbility("pickinferno")
+		end
+	end)
 end
