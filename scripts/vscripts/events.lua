d2a5eb8216d0f45ed76aa231a121a505a408c8f6
Teddeh Miracle <49321866+TeddehMiracle@users.noreply.github.com>
2020-05-12 16:50:56 -0400

    Update events.lua
    Added ember spirit to functions for doom (this is part of creating two different infernos)

--- scripts/vscripts/events.lua
+++ scripts/vscripts/events.lua
@@ -1,125 +1,133 @@
 require('libraries/notifications')
 
 function CTreeTagGameMode:OnNPCSpawned(keys) --All entity spawned will start here
-    DebugPrint("------------------------------------------------------------------")
-    local npc = EntIndexToHScript(keys.entindex)
-    local heroname = npc:GetUnitName()
-    DebugPrint("OnNPCSpawned:" .. heroname)
-    DebugPrintTable(keys)
-    
-    
-    
-    local tempdeadentcount = CTreeTagGameMode.DeadEntCount 
-    DebugPrint(heroname)
+	DebugPrint("------------------------------------------------------------------")
+	local npc = EntIndexToHScript(keys.entindex)
+	local heroname = npc:GetUnitName()
+	DebugPrint("OnNPCSpawned:" .. heroname)
+	DebugPrintTable(keys)
 
-    --function to remove excess dead ent from reviving due to bug 
-    if npc.bFirstSpawned == true then   
-        if heroname == "npc_dota_hero_earth_spirit" then
-           npc:SetRespawnsDisabled(true) 
-            CTreeTagGameMode.ExtraDeadEntCount = CTreeTagGameMode.ExtraDeadEntCount + 1
-           
-            
-            if CTreeTagGameMode.ExtraDeadEntCount > CTreeTagGameMode.DeadEntCount then
-                npc:Destroy()
-                DebugPrint("[BUG) Removed extra dead ent")
-                CTreeTagGameMode.ExtraDeadEntCount = CTreeTagGameMode.ExtraDeadEntCount - 1
-            elseif CTreeTagGameMode.ExtraDeadEntCount == CTreeTagGameMode.DeadEntCount then
-            end
-        elseif heroname == "npc_dota_hero_doom_bringer" then
-            CTreeTagGameMode.InfernoCount =  CTreeTagGameMode.InfernoCount + 1
-        end
-    end
+
+
+	local tempdeadentcount = CTreeTagGameMode.DeadEntCount 
+	DebugPrint(heroname)
+
+	--function to remove excess dead ent from reviving due to bug 
+	if npc.bFirstSpawned == true then   
+	if heroname == "npc_dota_hero_earth_spirit" then
+	   npc:SetRespawnsDisabled(true) 
+	    CTreeTagGameMode.ExtraDeadEntCount = CTreeTagGameMode.ExtraDeadEntCount + 1
+
+
+	    if CTreeTagGameMode.ExtraDeadEntCount > CTreeTagGameMode.DeadEntCount then
+		npc:Destroy()
+		DebugPrint("[BUG) Removed extra dead ent")
+		CTreeTagGameMode.ExtraDeadEntCount = CTreeTagGameMode.ExtraDeadEntCount - 1
+	    elseif CTreeTagGameMode.ExtraDeadEntCount == CTreeTagGameMode.DeadEntCount then
+	    end
+	elseif heroname == "npc_dota_hero_doom_bringer"
+	or heroname == "npc_dota_hero_ember_spirit" then
+	    CTreeTagGameMode.InfernoCount =  CTreeTagGameMode.InfernoCount + 1
+	end
+	end
+
+
+
+
+
+	--innate skill upgrade
+
+	local innateAbilityName = "inferno_tree_cutting"
+	local innateAbility2Name = "inferno_aoe_destroy_tree"
+	local innateAbility3Name = "invul"
+	local innateAbility4Name = "picktree"
+	local innateAbility5Name = "pickinferno"
+	local innateAbility6Name = "dailyinfgold"
+
+	--Ent innate skill 
+	if npc:IsRealHero() and npc:HasAbility(innateAbility3Name) then
+	npc:FindAbilityByName(innateAbility3Name):SetLevel(1)
+	end
+	if npc:IsRealHero() and npc:HasAbility(innateAbility4Name) then
+	npc:FindAbilityByName(innateAbility4Name):SetLevel(1)
+	end
+	if npc:IsRealHero() and npc:HasAbility(innateAbility5Name) then
+	npc:FindAbilityByName(innateAbility5Name):SetLevel(1)
+	end
     
-    
+	if npc.bFirstSpawned == nil then  --prevents skills from resetting
 
-    
+		--inferno innate skill
+		if npc:IsRealHero() and npc:HasAbility(innateAbilityName) then
+			npc:FindAbilityByName(innateAbilityName):SetLevel(1)
+		end
 
-    --innate skill upgrade
-  
-    local innateAbilityName = "inferno_tree_cutting"
-    local innateAbility2Name = "inferno_aoe_destroy_tree"
-    local innateAbility3Name = "invul"
-    local innateAbility4Name = "picktree"
-    local innateAbility5Name = "pickinferno"
-    local innateAbility6Name = "dailyinfgold"
+		if npc:IsRealHero() and npc:HasAbility(innateAbility2Name) then
+			npc:FindAbilityByName(innateAbility2Name):SetLevel(1)
+		end
 
-    --Ent innate skill 
-    if npc:IsRealHero() and npc:HasAbility(innateAbility3Name) then
-        npc:FindAbilityByName(innateAbility3Name):SetLevel(1)
-    end
-    if npc:IsRealHero() and npc:HasAbility(innateAbility4Name) then
-        npc:FindAbilityByName(innateAbility4Name):SetLevel(1)
-    end
-    if npc:IsRealHero() and npc:HasAbility(innateAbility5Name) then
-        npc:FindAbilityByName(innateAbility5Name):SetLevel(1)
-    end
-    
-    if heroname == "npc_dota_hero_doom_bringer" then 
-        
-        if npc.bFirstSpawned == nil then  --prevents skills from resetting
-            
-            --inferno innate skill
-            if npc:IsRealHero() and npc:HasAbility(innateAbilityName) then
-                npc:FindAbilityByName(innateAbilityName):SetLevel(1)
-            end
-                
-            if npc:IsRealHero() and npc:HasAbility(innateAbility2Name) then
-                npc:FindAbilityByName(innateAbility2Name):SetLevel(1)
-            end
+		if npc:IsRealHero() and npc:HasAbility(innateAbility6Name) then
+			npc:FindAbilityByName(innateAbility6Name):SetLevel(1)
+		end
+	end
 
-            if npc:IsRealHero() and npc:HasAbility(innateAbility6Name) then
-                npc:FindAbilityByName(innateAbility6Name):SetLevel(1)
-            end
-        end
-        --Prevents inferno from getting stuck when spawned 
+	if heroname == "npc_dota_hero_doom_bringer"
+	or heroname == "npc_dota_hero_ember_spirit" then 
+	--Prevents inferno from getting stuck when spawned 
 
-        local position = npc:GetAbsOrigin()
-        FindClearSpaceForUnit(npc, position, true)
-	
-	--Set Inferno model to Warlock Golem
-	npc:SetOriginalModel("models/heroes/warlock/warlock_demon.vmdl")
-	npc:SetModel("models/heroes/warlock/warlock_demon.vmdl")
-    end
+	local position = npc:GetAbsOrigin()
+	FindClearSpaceForUnit(npc, position, true)
+	end
+
+	if heroname == "npc_dota_hero_doom_bringer" then
+		npc:SetOriginalModel("models/heroes/warlock/warlock_demon.vmdl")
+		npc:SetModel("models/heroes/warlock/warlock_demon.vmdl")
+	end
+
+	if heroname == "npc_dota_hero_ember_spirit" then
+		npc:SetOriginalModel("models/heroes/ember_spirit/ember_spirit.vmdl")
+		npc:SetModel("models/heroes/ember_spirit/ember_spirit.vmdl")
+	end
         
    
 
-    --Reroute to OnHeroIngame in Addongamemode
-    if npc:IsRealHero() and npc.bFirstSpawned == nil then --unit only enters when spawned for first time
-        npc.bFirstSpawned = true
-        CTreeTagGameMode:OnHeroInGame(npc)
-        
-    end
-   
-    --===================================================================PRESTART FUNCTION AREA =================================================================
-    --Run Function At The Start of the game
-    if CTreeTagGameMode.RunAtStart == false then 
-        CTreeTagGameMode.RunAtStart = true 
+	--Reroute to OnHeroIngame in Addongamemode
+	if npc:IsRealHero() and npc.bFirstSpawned == nil then --unit only enters when spawned for first time
+	npc.bFirstSpawned = true
+	CTreeTagGameMode:OnHeroInGame(npc)
 
-        CTreeTagGameMode:PreStart()
-        --Put your function that wants to be run at bottom
-        if CTreeTagGameMode.StartVictoryTimer == 0 then 
-            CTreeTagGameMode.StartVictoryTimer = CTreeTagGameMode.StartVictoryTimer + 1
-        
-            StartVictoryTimer()
+	end
 
-        end
+	--===================================================================PRESTART FUNCTION AREA =================================================================
+	--Run Function At The Start of the game
+	if CTreeTagGameMode.RunAtStart == false then 
+	CTreeTagGameMode.RunAtStart = true 
 
-       StartMessagePopup()
-    end
-    if heroname == "npc_treetag_building_wall_1"
+	CTreeTagGameMode:PreStart()
+	--Put your function that wants to be run at bottom
+	if CTreeTagGameMode.StartVictoryTimer == 0 then 
+	    CTreeTagGameMode.StartVictoryTimer = CTreeTagGameMode.StartVictoryTimer + 1
+
+	    StartVictoryTimer()
+
+	end
+
+	StartMessagePopup()
+	end
+	if heroname == "npc_treetag_building_wall_1"
 	or heroname == "npc_treetag_building_sentrytower_1"
 	or heroname == "npc_treetag_building_bird_of_life"
 	or heroname == "npc_treetag_building_barracks"
 	or heroname == "npc_treetag_building_invisible_wall"
 	or heroname == "npc_treetag_building_mine_1"
 	then
-		
+
 		npc:SetHealth(1)
 		local base_regen = npc:GetBaseHealthRegen()
 		local temp_regen = 40
 		local regen_duration = (npc:GetMaxHealth()/temp_regen) + 1
 		npc:SetBaseHealthRegen(temp_regen)
-		
+
 		--Make abilities cooldown while building constructing
 		if npc:HasAbility("levelmine_2") then
 			ability = npc:FindAbilityByName("levelmine_2")
@@ -157,8 +165,8 @@ function CTreeTagGameMode:OnNPCSpawned(keys) --All entity spawned will start her
 			ability:SetHidden(false)
 			ability:SetActivated(true)
 		end
-		
-		
+
+
 		Timers:CreateTimer(regen_duration, function()
 				npc:SetBaseHealthRegen(base_regen)
 		end)
@@ -293,7 +301,8 @@ function CTreeTagGameMode:UnitLevelUp(keys)
 	local heroname = hero:GetUnitName()
 	DebugPrint(heroname)
 	--update choptree skill for inferno at certain levels
-	if heroname == "npc_dota_hero_doom_bringer" then
+	if heroname == "npc_dota_hero_doom_bringer"
+	or heroname == "npc_dota_hero_ember_spirit" then
 		if 	keys.level > 4 then
 			hero:FindAbilityByName("inferno_tree_cutting"):SetLevel(4)
 		elseif keys.level > 3 then
@@ -313,35 +322,50 @@ function checkinfernovictory()
 end
 
 function StartVictoryTimer()
-    --Starts victory timer for ent. Its 35 minutes for now
-    --Total game time : 35 minutes + extra 5 seconds
-    -- 35 mins plus extra mins =2105 sec
-    Timers:CreateTimer(2105, function()
-        GameRules:SetGameWinner(DOTA_TEAM_GOODGUYS)
-        DebugPrint("Dota Team GOod guy has won due to time running out")
-        local currentGameTime = GameRules:GetDOTATime(false,false)
-        DebugPrint(currentGameTime)
-    end)
+	--Starts victory timer for ent. Its 35 minutes for now
+	--Total game time : 35 minutes + extra 5 seconds
+	-- 35 mins plus extra mins =2105 sec
+	Timers:CreateTimer(2105, function()
+		GameRules:SetGameWinner(DOTA_TEAM_GOODGUYS)
+		DebugPrint("Dota Team GOod guy has won due to time running out")
+		local currentGameTime = GameRules:GetDOTATime(false,false)
+		DebugPrint(currentGameTime)
+	end)
 
-    --Give inferno special power at last 2.5 min. Placed here just because i lazy lol
-    --1805 refer to min 32.5 when he get the power up ... 1955 for reference
-    Timers:CreateTimer(1955, function()
-        local allinferno = Entities:FindAllByClassname("npc_dota_hero_doom_bringer")
-        DebugPrint("Inferno Special Power Activated")
-        for _, infernos in pairs(allinferno) do
-            DebugPrint("Accessed inferno endgame")
-            infernos:AddAbility("inferno_endgame")
-            ability = infernos:FindAbilityByName("inferno_endgame")
-            ability:SetLevel(1)
-           
-            for i = 0,30,1 
-            do 
-                infernos:HeroLevelUp(false)
-            end
-            
-        end
-        
-    end)
+	--Give inferno special power at last 2.5 min. Placed here just because i lazy lol
+	--1805 refer to min 32.5 when he get the power up ... 1955 for reference
+	Timers:CreateTimer(1955, function()
+		local allinferno = Entities:FindAllByClassname("npc_dota_hero_doom_bringer")
+		DebugPrint("Inferno Special Power Activated")
+		for _, infernos in pairs(allinferno) do
+		    DebugPrint("Accessed inferno endgame")
+		    infernos:AddAbility("inferno_endgame")
+		    ability = infernos:FindAbilityByName("inferno_endgame")
+		    ability:SetLevel(1)
+
+		    for i = 0,30,1 
+		    do 
+			infernos:HeroLevelUp(false)
+		    end
+
+		end
+
+		allinferno = Entities:FindAllByClassname("npc_dota_hero_ember_spirit")
+		DebugPrint("Burnin Special Power Activated")
+		for _, infernos in pairs(allinferno) do
+		    DebugPrint("Accessed inferno endgame")
+		    infernos:AddAbility("inferno_endgame")
+		    ability = infernos:FindAbilityByName("inferno_endgame")
+		    ability:SetLevel(1)
+
+		    for i = 0,30,1 
+		    do 
+			infernos:HeroLevelUp(false)
+		    end
+
+		end
+
+	end)
     
 end
 
